e0788a2b4bfc19d76ea4f7d05d893a4e33aa969f
AlinafeICK <alinafeick@gmail.com>
2024-02-29 13:40:20 +0200

    Amendment on _Own_Exit.c

--- _Own_Exit.c
+++ _Own_Exit.c
@@ -2,18 +2,28 @@
 
 /**
  * _Own_Exit - couses normal process termination
- * @args: empty args
+ * @args: Array of stringgs rep command & Arguments
  *
  * Return: 0 to terminate the process
  */
 int _Own_Exit(char **args)
 {
-	if (args[1])
+	int Status_Code;
+
+	if (args[1] != NULL)
 	{
-		return (atoi(args[1]));
+		Status_Code = atoi(args[1]);
+		if (Status_Code >= 0 && Status_Code <= 255)
+		{
+			exit(Status_Code);
+		}
+		else
+		{
+			printf("Invalid exit status code\n");
+		}
 	}
 	else
 	{
-		return (0);
+		exit(0);
 	}
 }
