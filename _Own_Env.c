4660e42a0da6e9fc8c1eb2dfd9c193fd17b2cdb1
Johntechcomputing <johntechcomputing@gmail.com>
2024-03-01 22:34:49 +0300

    Shell updates

--- _Own_Env.c
+++ _Own_Env.c
@@ -1,21 +1,39 @@
 #include "Shell.h"
 
 /**
- * _Own_Env - Function to print enviromental variables
- * @args: Arguments
+ * _Execute_Args - map if command is a builtin or a process
+ * @args: Command & its flags
  *
- * Return: 1 on success, else 0
+ * Return: 1 on sucess, 0 otherwise
  */
-int _Own_Env(char **args)
+int _Execute_Args(char **args)
 {
-	int i = 0;
-	(void)(**args);
+unsigned long int i = 0;
+char *Builtin_Functions_List[] = {
+"cd",
+"env",
+"help",
+"exit"
+};
+int (*Builtin_Functions[])(char **) = {
+&_Own_Cd,
+&_Own_Env,
+&_Own_Help,
+&_Own_Exit
+};
 
-	while (environ[i])
-	{
-		write(STDOUT_FILENO, environ[i], strlen(environ[i]));
-		write(STDOUT_FILENO, "\n", 1);
-		i++;
-	}
-	return (-1);
+if (args[0] == NULL)
+{
+return (-1);
 }
+for (; i < sizeof(Builtin_Functions_List) / sizeof(char *); i++)
+{
+if (strcmp(args[0], Builtin_Functions_List[i]) == 0)
+{
+(*Builtin_Functions[i])(args);
+return (0);
+}
+}
+return (_New_Process(args));
+}
+
